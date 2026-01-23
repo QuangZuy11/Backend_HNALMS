012ac1b7c87ef3cfc6f6dac2db1e3df1f651ecab
truongbabiboi1204 <truongcvhe176330@fpt.edu.vn>
2026-01-23 23:12:06 +0700

    add role acountent

--- src/modules/authentication/models/user.model.js
+++ src/modules/authentication/models/user.model.js
@@ -6,7 +6,7 @@ const UserSchema = new Schema({
     fullname: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    role: { type: String, enum: ['admin', 'manager', 'owner', 'tenant'], default: 'tenant' },
+    role: { type: String, enum: ['admin', 'manager', 'owner', 'tenant', 'accountant'], default: 'tenant' },
     avatarURL: { type: String },
     status: { type: String, enum: ['active', 'inactive', 'locked'], default: 'active' }
 }, { timestamps: true });
