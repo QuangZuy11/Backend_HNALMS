e8de4c2ab817a25d59add9a804e2f5052e586654
Nguyễn Hoàng Phúc <phucnhhe173073@fpt.edu.vn>
2026-02-03 18:21:56 +0700

    Add contract model

--- src/modules/contract-management/models/contract.model.js
+++ src/modules/contract-management/models/contract.model.js
@@ -1,1 +1,50 @@
-// Schema: tenant_id, room_id, start_date, end_date, price, deposit, status
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const contractSchema = new Schema(
+  {
+    contractCode: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    roomId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Room",
+      required: true,
+    },
+    tenantId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    depositCode: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Deposits",
+    },
+    personInRoom: {
+      type: Number,
+      default: 1,
+    },
+    startDate: {
+      type: Date,
+      required: true,
+    },
+    endDate: {
+      type: Date,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["active", "inactive"],
+      default: "active",
+    },
+    image: [String],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Contract = mongoose.model("Contracts", contractSchema, "contracts");
+module.exports = Contract;
