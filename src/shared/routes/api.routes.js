d26cc969860276be32809b259d3965eefe6a68d6
truongbabiboi1204 <truongcvhe176330@fpt.edu.vn>
2026-02-17 23:52:53 +0700

    add feater request repair

--- src/shared/routes/api.routes.js
+++ src/shared/routes/api.routes.js
@@ -37,11 +37,13 @@ ApiRouter.get("/users", async (req, res) => {
 const roomRoutes = require("../../modules/room-floor-management/routes/room-floor.routes");
 const buildingRoutes = require("../../modules/building-information/routes/building.routes");
 const serviceRoutes = require("../../modules/service-management/routes/service.routes");
+const requestRoutes = require("../../modules/request-management/routes/request.routes");
 
 // Mount routes
 ApiRouter.use("/auth", authRoutes);
 ApiRouter.use("/", roomRoutes);
 ApiRouter.use("/buildings", buildingRoutes);
 ApiRouter.use("/services", serviceRoutes);
+ApiRouter.use("/requests", requestRoutes);
 
 module.exports = ApiRouter;
