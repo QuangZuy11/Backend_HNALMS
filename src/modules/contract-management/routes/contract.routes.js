2a9de9a0d1d8744a16b9f63a4c63abcf79650dc8
Nguyễn Hoàng Phúc <phucnhhe173073@fpt.edu.vn>
2026-03-02 13:57:31 +0700

    code routes view my contract tenant

--- src/modules/contract-management/routes/contract.routes.js
+++ src/modules/contract-management/routes/contract.routes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const contractController = require("../controllers/contract.controller");
 const uploadContractImg = require("../middlewares/uploadContractImg");
+const { authenticate } = require("../../authentication/middlewares/authenticate");
 
 // Route to upload contract images to Cloudinary
 router.post("/upload-images", uploadContractImg.array("images", 5), contractController.uploadContractImages);
@@ -13,6 +14,7 @@ router.post("/create", contractController.createContract);
 
 // Route to get all contracts
 router.get("/", contractController.getAllContracts);
+router.get("/my-contracts", authenticate, contractController.getMyContracts);
 router.get("/:id", contractController.getContractById);
 
 module.exports = router;
