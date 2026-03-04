a5d4eb65370d803fa2fca9def3286cf7f35236fc
DaiNho <98211436+DaiNho@users.noreply.github.com>
2026-03-04 23:24:04 +0700

    thêm trang sửa hợp đồng cho manager, owner chỉ xem hợp đồng (readOnly)

--- src/modules/contract-management/routes/contract.routes.js
+++ src/modules/contract-management/routes/contract.routes.js
@@ -2,10 +2,16 @@ const express = require("express");
 const router = express.Router();
 const contractController = require("../controllers/contract.controller");
 const uploadContractImg = require("../middlewares/uploadContractImg");
-const { authenticate } = require("../../authentication/middlewares/authenticate");
+const {
+  authenticate,
+} = require("../../authentication/middlewares/authenticate");
 
 // Route to upload contract images to Cloudinary
-router.post("/upload-images", uploadContractImg.array("images", 5), contractController.uploadContractImages);
+router.post(
+  "/upload-images",
+  uploadContractImg.array("images", 5),
+  contractController.uploadContractImages,
+);
 
 // Route to create a new contract
 router.post("/create", contractController.createContract);
@@ -16,5 +22,6 @@ router.post("/create", contractController.createContract);
 router.get("/", contractController.getAllContracts);
 router.get("/my-contracts", authenticate, contractController.getMyContracts);
 router.get("/:id", contractController.getContractById);
+router.put("/:id", contractController.updateContract);
 
 module.exports = router;
