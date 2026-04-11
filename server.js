48f4bb2dde6223dcda244ab42f43cdb83607c3f1
Nguyễn Hoàng Phúc <phucnhhe173073@fpt.edu.vn>
2026-04-12 02:53:37 +0700

    Update server

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const emailService = require("./src/modules/notification-management/services/ema
 const { startDepositExpirationJob } = require("./src/shared/jobs/deposit-expiration.job");
 const contractStartJob = require("./src/modules/contract-management/jobs/contract-start.job");
 const { contractRenewalJob } = require("./src/modules/contract-management/jobs/contract-renewal.job");
+const { moveOutAutoCompleteJob } = require("./src/modules/contract-management/jobs/moveout-auto-complete.job");
 
 const PORT = process.env.PORT || 9999;
 
@@ -16,4 +17,5 @@ app.listen(PORT, async () => {
     startDepositExpirationJob();
     contractStartJob();
     contractRenewalJob();
+    moveOutAutoCompleteJob();
 });
